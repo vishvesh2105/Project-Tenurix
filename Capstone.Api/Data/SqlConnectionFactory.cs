8eb763fd412b07e5fd5f51dd6bd036a29be48475
Priyank Italiya <priyankitaliya89@gmail.com>
2026-01-29 21:14:01 -0500

    setup sql connection factory and password hasher

---
+++ Capstone.Api/Data/SqlConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Capstone.Api.Data;
+
+public sealed class SqlConnectionFactory
+{
+    private readonly string _cs;
+
+    public SqlConnectionFactory(IConfiguration config)
+    {
+        _cs = config.GetConnectionString("AzureSql")
+              ?? throw new InvalidOperationException("Missing ConnectionStrings:AzureSql");
+    }
+
+    public SqlConnection Create() => new SqlConnection(_cs);
+}
